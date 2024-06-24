5f83cf7c60638623d245054f6492835d7ce8aee5
habtshaa <Habtsha2021@gmail.com>
2024-06-24 10:33:41 +0300

    update

--- resources/views/employees/clearance.blade.php
+++ resources/views/employees/clearance.blade.php
@@ -1,5 +1,6 @@
 @include('partials.header')
 <body>
+  
 <nav class="sidebar close">
   <header>
     <div class="image-text">
@@ -7,43 +8,43 @@
         <img src="su/v.png" alt="">
       </span>
       <div class="text logo-text">
-        <span class="name">habtamu bitew </span>
+        <span class="name">habtamu bitew</span>
         <span class="profession">Employee {{ session('employee_id') }}</span>
       </div>
     </div>
-    <i class='bx bx-chevron-right toggle'></i>
+    <i class='material-icons toggle'>chevron_right</i>
   </header>
   <div class="menu-bar">
     <div class="menu">
       <ul class="menu-links">
         <li class="nav-link">
           <a href="#">
-            <i class='bx bx-home-alt icon'></i>
+            <i class='material-icons icon'>home</i>
             <span class="text nav-text">Home</span>
           </a>
         </li>
         <li class="nav-link">
           <a href="#">
-            <i class='bx bx-home-alt icon'></i>
+            <i class='material-icons icon'>account_circle</i>
             <span class="text nav-text">My profile</span>
           </a>
         </li>
         <li class="nav-link">
           <a href="{{url('emp')}}">
-            <i class='bx bx-home-alt icon'></i>
+            <i class='material-icons icon'>assignment</i>
             <span class="text nav-text">Request Form</span>
           </a>
         </li>
         <li class="nav-link">
           <a href="{{url('clearance')}}">
-            <i class='bx bx-home-alt icon'></i>
+            <i class='material-icons icon'>check_circle</i>
             <span class="text nav-text">My Clearance</span>
           </a>
         </li>
         <li class="nav-link">
-          <a href="#">
-            <i class='bx bx-home-alt icon'></i>
-            <span class="text nav-text">Help</span>
+          <a href="{{url('clearance-info')}}">
+            <i class='material-icons icon'>info</i>
+            <span class="text nav-text">Requirements</span>
           </a>
         </li>
       </ul>
@@ -51,92 +52,93 @@
     <div class="bottom-content">
       <li class="">
         <a href="{{url('/logout')}}">
-          <i class='bx bx-log-out icon'></i>
+          <i class='material-icons icon'>logout</i>
           <span class="text nav-text">Logout</span>
         </a>
       </li>
     </div>
   </div>
 </nav>
-<section class="home">
-  @if(session('success'))
-  <div class="alert alert-success">{{ session('success') }}</div>
-@elseif(session('error'))
-  <div class="alert alert-danger">{{ session('error') }}</div>
-@endif
-<div class="container">
-    <div class="table-wrapper">
-        <div class="table-title">
-            <div class="row">
-                <div class="col-sm-6">
-                    <h2><b>Clearance Status</b></h2>
-                </div>
-                <div class="col-sm-6">
-                  <td>
-                    <a id="applyButton" href="#addEmployeeModal" class="btn btn-success" data-toggle="modal">
-                        <i class="material-icons">&#xE147;</i> <span>Apply</span>
-                    </a>
-                </td>
-                                </div>
-            </div>
-        </div>
-        
-        <table class="table table-striped table-hover">
-          <thead>
-              <tr> 
-                  <th>Stakeholder </th>
-                  <th>Location </th>
-                  <th>Status</th>
-              </tr>
-          </thead>
-          <tbody>
-            @foreach ($stakeholderLocations as $stakeloc)
-                <tr>
-                    @php
-                        $stakeholder = $stakeholders->firstWhere('StakeholderID', $stakeloc->StakeholderID);
-                        $location = $locations->firstWhere('LocationID', $stakeloc->LocationID);
-                        $stakeholderName = $stakeholder ? $stakeholder->Workdep : '';
-                        $locationName = $location ? $location->LocationName : '';
-                    @endphp
-                    <td>{{ $stakeholderName }}</td>
-                    <td>{{ $locationName }}</td>
-                    @foreach ($clearanceForms as $clearanceForm)
-                        <td>
-                            @foreach ($clearanceApprovals as $cp)
-                                @if ($cp->StakeholderLocationID == $stakeloc->StakeholderLocationID && $cp->ClearanceFormID == $clearanceForm->ClearanceFormID)
-                                    @switch($cp->ApprovalStatus)
-                                        @case('Approved')
-                                            <a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal">
-                                                <span>{{ $cp->ApprovalStatus }}</span>
-                                            </a>
-                                            @break
-                                        @case('Denied')
-                                            <a href="#addEmployeeModal" class="btn btn-danger" data-toggle="modal">
-                                                <span>{{ $cp->ApprovalStatus }}</span>
-                                            </a>
-                                            @break
-                                        @case('Pending')
-                                            <a href="#addEmployeeModal" class="btn btn-warning" data-toggle="modal">
-                                                <span>{{ $cp->ApprovalStatus }}</span>
-                                            </a>
-                                            @break
-                                        
-                                    @endswitch
-                                    
-                                @endif
-                            @endforeach
-                            
-                        </td>
-                    @endforeach
-                </tr>
-            @endforeach
-           
-        </tbody>
-        
-    </div>
-</div>
 
+<section class="home">
+
+    @if(session('success'))
+        <div class="alert alert-success">{{ session('success') }}</div>
+    @elseif(session('error'))
+        <div class="alert alert-danger">{{ session('error') }}</div>
+    @endif
+
+    <div class="container">
+        <div class="table-wrapper">
+            <div class="table-title">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <h2><b>Clearance Status</b></h2>
+                    </div>
+                    <div class="col-sm-6 text-right">
+                        <button id="applyButton" class="btn btn-success" data-toggle="modal" data-target="#addEmployeeModal">
+                            <i class="material-icons">&#xE147;</i> Apply
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <table class="table table-striped table-hover table-bordered">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">Stakeholder</th>
+                        <th scope="col">Location</th>
+                        <th scope="col">Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($stakeholderLocations as $stakeloc)
+                        <tr>
+                            @php
+                                $stakeholder = $stakeholders->firstWhere('StakeholderID', $stakeloc->StakeholderID);
+                                $location = $locations->firstWhere('LocationID', $stakeloc->LocationID);
+                                $stakeholderName = $stakeholder ? $stakeholder->Workdep : '';
+                                $locationName = $location ? $location->LocationName : '';
+                            @endphp
+                            <td>{{ $stakeholderName }}</td>
+                            <td>{{ $locationName }}</td>
+                            <td>
+                                @foreach ($clearanceForms as $clearanceForm)
+                                    @foreach ($clearanceApprovals as $cp)
+                                        @if ($cp->StakeholderLocationID == $stakeloc->StakeholderLocationID && $cp->ClearanceFormID == $clearanceForm->ClearanceFormID)
+                                            @switch($cp->ApprovalStatus)
+                                                @case('Approved')
+                                                    <button class="btn btn-success btn-sm">
+                                                        <span>{{ $cp->ApprovalStatus }}</span>
+                                                    </button>
+                                                    @break
+                                                @case('Denied')
+                                                    <button class="btn btn-danger btn-sm">
+                                                        <span>{{ $cp->ApprovalStatus }}</span>
+                                                    </button>
+                                                    @break
+                                                @case('Pending')
+                                                    <button class="btn btn-warning btn-sm">
+                                                        <span>{{ $cp->ApprovalStatus }}</span>
+                                                    </button>
+                                                    @break
+                                                @default
+                                                    <button class="btn btn-secondary btn-sm">
+                                                        <span>{{ $cp->ApprovalStatus }}</span>
+                                                    </button>
+                                            @endswitch
+                                        @endif
+                                    @endforeach
+                                @endforeach
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 </section>
+
 <div id="addEmployeeModal" class="modal fade">
   <div class="modal-dialog">
       <div class="modal-content">
